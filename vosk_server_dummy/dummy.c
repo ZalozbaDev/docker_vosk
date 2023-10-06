3348eeffc159925ee99befc97000e1eeb227af8d
Daniel Zoba <daniel.sobe@sorben.com>
2023-10-06 11:28:47 +0200

    update dummy to show useful recognition results

--- vosk_server_dummy/dummy.c
+++ vosk_server_dummy/dummy.c
@@ -18,6 +18,17 @@ static VoskRecognizer dummyRecognizer;
 
 static int silence_ctr = 0;
 
+const char * resultTexts[] = {
+	"{ \"partial\" : \"Ja\" }",
+	"{ \"partial\" : \"Ja sam\" }",
+        "{ \"partial\" : \"Ja sym tu sym\" }",
+        "{ \"partial\" : \"Ja sym tawzynt dušny\" }",
+        "{ \"partial\" : \"Ja sym tawzynt dušny karta\" }",
+        "{ \"text\" : \"Ja sym tawzynt dušny kadla.\" }"
+};
+
+#define result_array_size (sizeof(resultTexts) / sizeof (const char *))
+
 VoskModel *vosk_model_new(const char *model_path)
 {
 	printf("vosk_model_new, path=%s.\n", model_path);
@@ -53,7 +64,7 @@ void vosk_recognizer_set_words(VoskRecognizer *recognizer, int words)
 int vosk_recognizer_accept_waveform(VoskRecognizer *recognizer, const char *data, int length)
 {
 	printf("vosk_recognizer_accept_waveform, length=%d.\n", length);
-	if (silence_ctr < 5)
+	if (silence_ctr < (result_array_size - 1))
 	{
 		silence_ctr++;
 		printf("vosk_recognizer_accept_waveform --> continue decoding\n");
@@ -64,26 +75,26 @@ int vosk_recognizer_accept_waveform(VoskRecognizer *recognizer, const char *data
 	return 1;
 }
 
-const char *result_text="{ \"text\" : \"Result text\" }";
+// const char *result_text="{ \"text\" : \"Result text\" }";
 
 const char *vosk_recognizer_result(VoskRecognizer *recognizer)
 {
-	printf("vosk_recognizer_result\n");
-	return result_text;
+	printf("vosk_recognizer_result: %s\n", resultTexts[result_array_size - 1]);
+	return resultTexts[result_array_size - 1];
 }
 
-const char *partial_result_text="{ \"partial\" : \"Partial result text\" }";
+// const char *partial_result_text="{ \"partial\" : \"Partial result text\" }";
 
 const char *vosk_recognizer_partial_result(VoskRecognizer *recognizer)
 {
-	printf("vosk_recognizer_partial_result\n");
-	return partial_result_text;
+	printf("vosk_recognizer_partial_result: %s\n", resultTexts[silence_ctr - 1]);
+	return resultTexts[silence_ctr];
 }
 
 const char *final_result_text="{ \"text\" : \"Final result text\" }";
 
 const char *vosk_recognizer_final_result(VoskRecognizer *recognizer)
 {
-	printf("vosk_recognizer_final_result\n");
+	printf("vosk_recognizer_final_result: %s\n", final_result_text);
 	return final_result_text;
 }
