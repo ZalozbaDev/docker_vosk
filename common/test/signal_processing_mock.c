3ba24fc535b570f6f2070df96546a06b4b92db8b
Daniel Zoba <daniel.sobe@sorben.com>
2024-06-10 11:57:16 +0000

    add tests for output of partial result

--- common/test/signal_processing_mock.c
+++ common/test/signal_processing_mock.c
@@ -10,11 +10,15 @@ void WebRtcSpl_ResetResample48khzTo16khz(WebRtcSpl_State48khzTo16khz* state)
 	
 }
 
+// downsampling without filtering
 void WebRtcSpl_Resample48khzTo16khz(const int16_t* in,
                                     int16_t* out,
                                     WebRtcSpl_State48khzTo16khz* state,
                                     int32_t* tmpmem)
 {
-	
+	for (int i = 0; i < 480; i += 3)
+	{
+		out[i / 3] = in[i];
+	}
 }
 
--- common/test/tests_voskrecognizer_whisper.cpp
+++ common/test/tests_voskrecognizer_whisper.cpp
@@ -6,10 +6,41 @@
 #include <stddef.h>
 #include <stdint.h>
 
-TEST_CASE("startup")
+void fill_buffer(int16_t* buf, size_t valOffset, size_t len)
+{
+	for (size_t index = 0; index < len; index++)
+	{
+		buf[index] = (index / 3) + valOffset;	
+	}
+}
+
+TEST_CASE("partial and full result format output options")
 {	
-	SUBCASE("run with 1 frame of samples") {
-		VoskRecognizer *vosk = new VoskRecognizer(1, 48000, "dummyConfig");
+	SUBCASE("simple partial result (empty)") {
+		char dummyData[16000];
+		VoskRecognizer vosk(1, 48000, "dummyConfig");
+		vosk.setDetailedResult(false);
+		vosk.acceptWaveform(dummyData, sizeof(dummyData));
+		CHECK(std::string(vosk.getPartialResult()).compare("{ \"partial\" : \"\" }") == 0);
+	}
+
+	SUBCASE("detailed partial result (empty), recognizer inactive") {
+		char dummyData[16000] = {0};
+		VoskRecognizer vosk(1, 48000, "dummyConfig");
+		vosk.setDetailedResult(true);
+		vosk.acceptWaveform(dummyData, sizeof(dummyData));
+		CHECK(std::string(vosk.getPartialResult()).compare("{ \"partial\" : \"\", \"listen\" : \"false\" }") == 0);
+	}
+
+	SUBCASE("detailed partial result (empty), recognizer active") {
+		char dummyData[48000] = {127};
+		VoskRecognizer vosk(1, 48000, "dummyConfig");
+		vosk.setDetailedResult(true);
+		vosk.acceptWaveform(dummyData, sizeof(dummyData));
+		fill_buffer((int16_t *) &dummyData, 0, sizeof(dummyData) / 2);
+		WebRtcVad_Mock_set_result(1);
+		vosk.acceptWaveform(dummyData, sizeof(dummyData) / 2);
+		CHECK(std::string(vosk.getPartialResult()).compare("{ \"partial\" : \"\", \"listen\" : \"true\" }") == 0);
 	}
 
 
