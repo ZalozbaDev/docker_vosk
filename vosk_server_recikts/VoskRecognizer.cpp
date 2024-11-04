082335a770c26412bd1ce816c90a61e3bc127e1c
Daniel Zoba <daniel.sobe@sorben.com>
2024-11-04 14:38:20 +0100

    use default settings for a start

--- vosk_server_recikts/VoskRecognizer.cpp
+++ vosk_server_recikts/VoskRecognizer.cpp
@@ -52,7 +52,7 @@ VoskRecognizer::VoskRecognizer(int modelId, float sample_rate, const char *confi
 
     // init static parts already here
 
-    vad = new VADWrapper(3, m_processingSampleRate, 15, 15, 15, 5);
+    vad = new VADWrapper(3, m_processingSampleRate, 15, 15, 5, 5);
 	m_vadFrameCounter = 0;
     
     audioLogger = new AudioLogger(std::string(PREFIX "logs/"), m_instanceId);
