d5d1d78e6a3fe474cc0df22b80f01cdc6308ca78
Daniel Zoba <daniel.sobe@sorben.com>
2025-01-24 10:52:04 +0100

    successful accelarated build with ununtu 24.04

---
+++ common/cmdline/build_whisper_nvidia.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# do this manually and only once
+
+# git clone https://github.com/ZalozbaDev/webrtc-audio-processing.git webrtc-audio-processing
+# cd webrtc-audio-processing && git checkout 001d483d0c761f004e1888307acccc87d3520ec8
+
+# apt install -y meson libabsl-dev
+
+# cd webrtc-audio-processing && meson . build -Dprefix=$PWD/install && ninja -C build
+
+
+
+# git clone https://github.com/ZalozbaDev/whisper.cpp.git whisper.cpp
+
+# cd whisper.cpp && git checkout v1.7.4
+# cmake -B build -DGGML_CUDA=1 && cmake --build build -j --config Release
+
+# apt install -y libhunspell-dev  libicu-dev libsndfile1-dev
+
+
+rm -rf whisper_out/
+mkdir -p whisper_out/
+
+cp ../*.h ../*.cpp whisper_out/
+
+cp ../../vosk_server_whisper/VoskRecognizer.cpp ../../vosk_server_whisper/VoskRecognizer.h whisper_out/
+
+cp whisper.cpp/build/src/libwhisper.so.1 whisper_out/
+cp whisper.cpp/build/ggml/src/*.so       whisper_out/
+pushd whisper_out
+ln -s libwhisper.so.1 libwhisper.so
+popd
+
+g++ -Wall -Wno-write-strings -O3 -g3 -std=c++17 -O3 -fPIC -o whisper_out/whisper_main \
+-DVAD_FRAME_CONVERT_FLOAT \
+-Iwhisper_out/ -I. -Iwebrtc-audio-processing/webrtc/ -Iwhisper.cpp/ -Iwhisper.cpp/examples/ \
+-Iwhisper.cpp/include/ -Iwhisper.cpp/ggml/include/ \
+whisper_out/RecognizerBase.cpp \
+whisper_out/vosk_api_wrapper.cpp whisper_out/VoskRecognizer.cpp whisper_out/VADWrapper.cpp whisper_out/AudioLogger.cpp \
+whisper_out/HunspellPostProc.cpp whisper_out/CustomPostProc.cpp \
+main.cpp \
+webrtc-audio-processing/build/webrtc/common_audio/libcommon_audio.a \
+-ldl -lpthread -lhunspell -licuio -licuuc -lsndfile -lwhisper -lggml -lggml-cpu -lggml-base -Lwhisper_out/ 
+
