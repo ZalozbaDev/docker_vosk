7c4fccb830e6aebc27b5fa7b5a37655e411ed14d
Daniel Zoba <danielzoba@Mac.fritz.box>
2025-04-29 14:23:31 +0200

    remember buiulding whisper.cpp itself with coreml

---
+++ common/cmdline/build_whisper_coreml.sh
@@ -0,0 +1,9 @@
+
+# install CMake (via GUI or homebrew) and make sure that XCode cmdline utils are up to date
+
+
+# git clone https://github.com/ZalozbaDev/whisper.cpp.git whisper.cpp
+
+# cd whisper.cpp && git checkout v1.7.4
+# cmake -B build -DWHISPER_COREML=1 && cmake --build build -j --config Release
+
