0c5b5f02a32160e53ae802bdf6d7292b8eb7acf3
Daniel Zoba <daniel.sobe@sorben.com>
2025-08-07 08:50:22 +0200

    debugging help

--- vosk_server_recikts/startme.sh
+++ vosk_server_recikts/startme.sh
@@ -2,9 +2,13 @@
 
 LD_LIBRARY_PATH=/data/:/onnxruntime-linux-x64-1.12.1/lib/ /recikts_server 0.0.0.0 2700 1 ${MODEL_PATH_FULL}
 
-# export LD_LIBRARY_PATH=/data/
-# export VOSK_SAMPLE_RATE=48000
-# gdb /recikts_server
-# set args 0.0.0.0 2700 1 /merged.cfg
-# set print thread-events off
-# run
+# while :; do sleep 1; done
+
+if /bin/false; then
+	export LD_LIBRARY_PATH=/data/:/onnxruntime-linux-x64-1.12.1/lib/
+	export VOSK_SAMPLE_RATE=48000
+	gdb /recikts_server
+	set args 0.0.0.0 2700 1 /data/misa_2025_03_05.cfg
+	set print thread-events off
+	run
+fi
