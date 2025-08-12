9ba9d526bed339785da5b1403662906ddf13c046
Daniel Zoba <daniel.sobe@sorben.com>
2025-08-12 17:08:11 +0200

    make example work with resource as cmdline aRG

--- common/cmdline/cmdline_example_transcripts.sh
+++ common/cmdline/cmdline_example_transcripts.sh
@@ -1,10 +1,24 @@
-ffmpeg -i __INPUT_FILM__ audio_stripped.wav
+#!/bin/bash
 
-sox audio_stripped.wav -r 48000 -c 1 -b 16 audio_resampled.wav
+INPUTFILE=$1
 
-mkdir -p __OUTPUT_DIR__MUST_EXIST__
+echo "Creating transcript for $INPUTFILE"
 
-LD_LIBRARY_PATH=whisper.cpp/build/src/ ./whisper_out/whisper_main \
+rm -f ./tmp_audio_stripped.wav ./tmp_audio_stripped_resampled.wav
+rm -rf tmpoutdir/
+mkdir -p tmpoutdir/
+
+ffmpeg -i $INPUTFILE ./tmp_audio_stripped.wav
+
+sox ./tmp_audio_stripped.wav -r 48000 -c 1 -b 16 ./tmp_audio_stripped_resampled.wav
+
+LD_LIBRARY_PATH=whisper.cpp/build/src/:onnxruntime-linux-x64-1.12.1/lib/ ./whisper_out/whisper_main \
 ../../../whisper_models/Korla/whisper_large_v3_turbo_hsb/ggml-model.bin \
-audio_resampled.wav \
-__OUTPUT_DIR__MUST_EXIST__
+tmp_audio_stripped_resampled.wav \
+tmpoutdir/
+
+TRANSCRIPTFILE=$(echo $INPUTFILE | sed 's/\.[^./]\{3\}$/\.srt/')
+
+echo "Writing transcript file $TRANSCRIPTFILE"
+
+mv tmpoutdir/subtitles.srt $TRANSCRIPTFILE
