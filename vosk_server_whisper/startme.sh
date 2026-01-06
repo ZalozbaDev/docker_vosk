58ea5483e76b0b8ad3039064c1dfdc87655dbbbd
Daniel Zoba <d.zoba@zalozba.de>
2026-01-06 15:20:06 +0100

    update GDB instructions

--- vosk_server_whisper/startme.sh
+++ vosk_server_whisper/startme.sh
@@ -7,7 +7,8 @@ LD_LIBRARY_PATH=/:/onnxruntime-linux-x64-1.12.1/lib/ /vosk_whisper_server 0.0.0.
 # while /bin/true; do sleep 1 ; done
 
 # export VOSK_SAMPLE_RATE=48000 
+# export LD_LIBRARY_PATH=/:/onnxruntime-linux-x64-1.12.1/lib/
 # gdb /vosk_whisper_server
-# set args 0.0.0.0 2700 1 /whisper/Korla/hsb_stt_demo/hsb_whisper/ggml-model.q8_0.bin
+# set args 0.0.0.0 2700 1 /whisper/Korla/whisper_large_v3_turbo_hsb-0/ggml-model.bin
 # set print thread-events off
 # run
