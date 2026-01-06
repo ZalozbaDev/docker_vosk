0e170c71913f2fe3191aaf565024fe2a796a3331
Daniel Zoba <d.zoba@zalozba.de>
2026-01-06 10:22:24 +0100

    factor out whisper implementation

--- common/cmdline/build_whisper_nvidia.sh
+++ common/cmdline/build_whisper_nvidia.sh
@@ -30,7 +30,7 @@ mkdir -p whisper_out/
 
 cp ../*.h ../*.cpp whisper_out/
 
-cp ../../vosk_server_whisper/VoskRecognizer.cpp ../../vosk_server_whisper/VoskRecognizer.h whisper_out/
+cp ../../vosk_server_whisper/*.cpp ../../vosk_server_whisper/*.h whisper_out/
 
 # if you want whisper debug output, you must already build whisper with WHISPER_DEBUG set (check src/whisper.cpp!)
 
@@ -49,6 +49,7 @@ whisper_out/RecognizerBase.cpp \
 whisper_out/vosk_api_wrapper.cpp whisper_out/VoskRecognizer.cpp whisper_out/VADWrapperWebRTC.cpp whisper_out/VADWrapperSilero.cpp whisper_out/SileroVadIterator.cpp whisper_out/AudioLogger.cpp \
 whisper_out/ResamplerLibResample_48_16.cpp whisper_out/ResamplerWebRTC_48_16.cpp \
 whisper_out/HunspellPostProc.cpp whisper_out/CustomPostProc.cpp whisper_out/RepetitionRemover.cpp \
+whisper_out/WhisperImpl.cpp \
 main.cpp \
 webrtc-audio-processing/build/webrtc/common_audio/libcommon_audio.a \
 -ldl -lpthread -lhunspell -licuio -licuuc -lsndfile -lwhisper -lggml -lggml-cpu -lggml-base -lonnxruntime -lresample -Lwhisper_out/ -Lonnxruntime-linux-x64-1.12.1/lib/
--- vosk_server_whisper/VoskRecognizer.h
+++ vosk_server_whisper/VoskRecognizer.h
@@ -21,82 +21,8 @@ extern "C" {
 #include <RecognitionResult.h>
 #include <AudioLogger.h>
 
-#ifndef WHISPER_MOCK
-#include "whisper.h"
-#else
-#include "whisper_mock.h"
-#endif
-
 #include <HunspellPostProc.h>
 #include <CustomPostProc.h>
-
-// command-line parameters from whisper.cpp/examples/main/main.cpp
-struct whisper_params {
-    int32_t n_threads     = std::min(4, (int32_t) std::thread::hardware_concurrency());
-    int32_t n_processors  = 1;
-    int32_t offset_t_ms   = 0;
-    int32_t offset_n      = 0;
-    int32_t duration_ms   = 0;
-    int32_t progress_step = 5;
-    int32_t max_context   = -1;
-    int32_t max_len       = 0;
-    int32_t best_of       = whisper_full_default_params(WHISPER_SAMPLING_GREEDY).greedy.best_of;
-    int32_t beam_size     = whisper_full_default_params(WHISPER_SAMPLING_BEAM_SEARCH).beam_search.beam_size;
-    int32_t audio_ctx     = 0;
-
-    float word_thold      =  0.01f;
-    float entropy_thold   =  2.40f;
-    float logprob_thold   = -1.00f;
-    float grammar_penalty = 100.0f;
-    float temperature     = 0.0f;
-    float temperature_inc = 0.2f;
-
-    bool debug_mode      = false;
-    bool translate       = false;
-    bool detect_language = false;
-    bool diarize         = false;
-    bool tinydiarize     = false;
-    bool split_on_word   = false;
-    bool no_fallback     = false;
-    bool output_txt      = false;
-    bool output_vtt      = false;
-    bool output_srt      = false;
-    bool output_wts      = false;
-    bool output_csv      = false;
-    bool output_jsn      = false;
-    bool output_jsn_full = false;
-    bool output_lrc      = false;
-    bool no_prints       = false;
-    bool print_special   = false;
-    bool print_colors    = false;
-    bool print_progress  = false;
-    bool no_timestamps   = false;
-    bool log_score       = false;
-    bool use_gpu         = true;
-    bool flash_attn      = false;
-
-    std::string language  = "en";
-    std::string prompt;
-    std::string font_path = "/System/Library/Fonts/Supplemental/Courier New Bold.ttf";
-    std::string model     = "models/ggml-base.en.bin";
-    std::string grammar;
-    std::string grammar_rule;
-
-    // [TDRZ] speaker turn string
-    std::string tdrz_speaker_turn = " [SPEAKER_TURN]"; // TODO: set from command line
-
-    // A regular expression that matches tokens to suppress
-    std::string suppress_regex;
-
-    std::string openvino_encode_device = "CPU";
-
-    std::string dtw = "";
-
-    std::vector<std::string> fname_inp = {};
-    std::vector<std::string> fname_out = {};
-
-    // grammar_parser::parse_state grammar_parsed;
-};
 
 //////////////////////////////////////////////
 class VoskRecognizer:public RecognizerBase
@@ -142,7 +68,6 @@ private:
 
 	std::string m_configPath;
 
-	whisper_params default_params;
 	const int n_samples_30s  = (1e-3 * 30000.0) * WHISPER_SAMPLE_RATE;
     
 	std::vector<float> pcmf32;
@@ -159,6 +84,8 @@ private:
 	
 	char* leftOverData;
 	int leftOverDataLen = 0;
+	
+	WhisperImpl *whisperImpl;
 
 	void runTokensToWords(void);
 
@@ -185,12 +112,6 @@ private:
 	
 	HunspellPostProc *hpp;
 	CustomPostProc *cpp;
-	
-	// additional options from envvars
-	std::string env_vosk_model_language;
-	int         env_whisper_max_context;
-	bool        env_whisper_no_timestamps;
-	bool        env_whisper_no_fallback;
 };
 
 #endif // VOSK_RECOGNIZER_H
