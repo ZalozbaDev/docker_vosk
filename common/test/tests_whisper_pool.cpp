65605673ceac356dd9568fe313b3b37c4526421e
Daniel Zoba <d.zoba@zalozba.de>
2026-01-07 09:23:19 +0100

    test case with overallocation seems to work as well

--- common/test/tests_whisper_pool.cpp
+++ common/test/tests_whisper_pool.cpp
@@ -6,8 +6,14 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#include <thread>
+#include <chrono>
+
+#include <iostream>
+
 TEST_CASE("simple allocate/deallocate tests")
 {	
+	
 	SUBCASE("pool of one") {
 		whisper_mock_set_overload(false, false);
 		WhisperPool::setWhisperParams("ggml.bin", "auto", -1, false, true, false);
@@ -35,8 +41,48 @@ TEST_CASE("simple allocate/deallocate tests")
 		WhisperPool::releaseInstance(std::move(inst1));
 		WhisperPool::allocate(0);
 	}
-	
+}
+
+void concurrent_user() {
+	std::unique_ptr<WhisperImpl> inst;
+	std::cout << "concurrent_user: allocate" << std::endl;
+	inst = WhisperPool::getInstance();
+	std::this_thread::sleep_for(std::chrono::seconds(2));
+	std::cout << "concurrent_user: release" << std::endl;
+	WhisperPool::releaseInstance(std::move(inst));
+};
 	
 
+TEST_CASE("try overallocation")
+{	
+	SUBCASE("overallocate one") {
+		whisper_mock_set_overload(false, false);
+		WhisperPool::setWhisperParams("ggml.bin", "auto", -1, false, true, false);
+		WhisperPool::allocate(1);
+		std::thread t(concurrent_user);
+		std::unique_ptr<WhisperImpl> inst;
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+		inst = WhisperPool::getInstance();
+		WhisperPool::releaseInstance(std::move(inst));
+		t.join();
+		WhisperPool::allocate(0);
+	}
+	
+	SUBCASE("overallocate two") {
+		whisper_mock_set_overload(false, false);
+		WhisperPool::setWhisperParams("ggml.bin", "auto", -1, false, true, false);
+		WhisperPool::allocate(1);
+		std::thread t1(concurrent_user);
+		std::thread t2(concurrent_user);
+		std::unique_ptr<WhisperImpl> inst;
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+		inst = WhisperPool::getInstance();
+		WhisperPool::releaseInstance(std::move(inst));
+		t2.join();
+		t1.join();
+		WhisperPool::allocate(0);
+	}
+	
 }
+
 
