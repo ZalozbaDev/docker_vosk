3041b2b47aa86762bdaab2e4538b84a6151ffe41
Daniel Zoba <d.zoba@zalozba.de>
2026-08-06 11:28:31 +0200

    fix building cmdline apps

--- common/cmdline/build_recikts.sh
+++ common/cmdline/build_recikts.sh
@@ -24,7 +24,7 @@ g++ -Wall -Wno-write-strings -O3 -g3 -std=c++17 -fPIC -o recikts_out/recikts_mai
 -Ionnxruntime-linux-x64-1.12.1/include/ \
 recikts_out/RecognizerBase.cpp \
 recikts_out/vosk_api_wrapper.cpp recikts_out/VoskRecognizer.cpp recikts_out/VADWrapperWebRTC.cpp recikts_out/VADWrapperSilero.cpp recikts_out/SileroVadIterator.cpp recikts_out/AudioLogger.cpp \
-recikts_out/ResamplerLibResample_48_16.cpp recikts_out/ResamplerWebRTC_48_16.cpp recikts_out/RepetitionRemover.cpp \
+recikts_out/ResamplerLibResample_48_16.cpp recikts_out/ResamplerLibResample_8_16.cpp recikts_out/ResamplerWebRTC_48_16.cpp recikts_out/ResamplerWebRTC_8_16.cpp recikts_out/RepetitionRemover.cpp \
 recikts_out/HunspellPostProc.cpp recikts_out/CustomPostProc.cpp \
 recikts_out/RecIKTSImpl.cpp main.cpp \
 webrtc-audio-processing/build/webrtc/common_audio/libcommon_audio.a \
--- common/cmdline/build_whisper_noaccel.sh
+++ common/cmdline/build_whisper_noaccel.sh
@@ -37,8 +37,8 @@ g++ -Wall -Wno-write-strings -O3 -g3 -std=c++17 -O3 -fPIC -o whisper_out/whisper
 -Ionnxruntime-linux-x64-1.12.1/include/ \
 whisper_out/RecognizerBase.cpp \
 whisper_out/vosk_api_wrapper.cpp whisper_out/VoskRecognizer.cpp whisper_out/VADWrapperWebRTC.cpp whisper_out/VADWrapperSilero.cpp whisper_out/SileroVadIterator.cpp whisper_out/AudioLogger.cpp \
-whisper_out/ResamplerLibResample_48_16.cpp whisper_out/ResamplerWebRTC_48_16.cpp whisper_out/RepetitionRemover.cpp \
-whisper_out/HunspellPostProc.cpp whisper_out/CustomPostProc.cpp \
+whisper_out/ResamplerLibResample_48_16.cpp whisper_out/ResamplerLibResample_8_16.cpp whisper_out/ResamplerWebRTC_48_16.cpp whisper_out/ResamplerWebRTC_8_16.cpp whisper_out/RepetitionRemover.cpp \
+whisper_out/HunspellPostProc.cpp whisper_out/CustomPostProc.cpp whisper_out/PartialResultOracle.cpp \
 whisper_out/WhisperImpl.cpp whisper_out/WhisperPool.cpp main.cpp \
 webrtc-audio-processing/build/webrtc/common_audio/libcommon_audio.a \
 -ldl -lpthread -lhunspell -licuio -licuuc -lsndfile -lonnxruntime -lresample -Lonnxruntime-linux-x64-1.12.1/lib/ -lwhisper -lggml -lggml-cpu -lggml-base -Lwhisper_out/ 
